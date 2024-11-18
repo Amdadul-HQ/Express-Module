7a7c364f683748d6790a7425a7ebe0403399e4dc
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-18 15:56:25 +0600

    adding streaming

--- stream-buffer.js
+++ stream-buffer.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer();
 
 server.on("request",(req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method);
     res.end("Hello world")
+    if(req.url === '/read' && req.method === "GET"){
+        const readAbleStram = fs.createReadStream('./texts/read.txt')
+        readAbleStram.on("data",(buffer)=>{
+            res.write(buffer)
+        })
+        readAbleStram.on("end",()=>{
+            res.end("This data end here")
+        })
+    }
 })
 
 server.listen(5000,()=>{
