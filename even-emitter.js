58ec6bde4508894271e0b749af59cfc017f34e49
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-18 15:29:02 +0600

    add: emitter func

--- even-emitter.js
+++ even-emitter.js
@@ -1,1 +1,15 @@
 const eventsEmitter = require('events');
+
+// lister
+
+const myEmitter = new eventsEmitter();
+
+myEmitter.on("birthday",()=>{
+    console.log("happly birthday to you");
+})
+
+myEmitter.on("birthday",(gift)=>{
+    console.log(`i will send a ${gift}`);
+})
+
+myEmitter.emit("birthday","watch")
