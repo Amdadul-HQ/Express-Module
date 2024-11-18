ab64275135a18c687c3ae688327177126473521a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-18 15:25:53 +0600

    event emitter

--- asynchoronous.js
+++ asynchoronous.js
@@ -2,4 +2,13 @@ const fs = require('fs');
 
 // Reading file asynchoronously
 
-const 
+fs.readFile("./texts/read.txt","utf-8",(error,data)=>{
+    if(error){
+        throw Error("Error by reading data")
+    }
+    console.log(data);
+    fs.writeFile("./texts/write2.txt",data,"utf-8",(err)=>{
+            throw Error("Error by writeing")
+    })
+    
+})
