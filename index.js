60dfaf80eb1a24ca2771716c936e00d41696f2d5
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-18 14:47:21 +0600

    add: built in module

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const {add,a} = require('./local-1');
 
+
+
+
 console.log(add(1,2),a);
--- local-1.js
+++ local-1.js
@@ -1,5 +1,9 @@
+
+// local module
 const add = (param1,param2) =>  param1 + param2;
 
 const a = 10
 
 module.exports = {add,a};
+
+// built in module
